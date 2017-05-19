9b8c9e84dd924e948858769266b4dcd669bb5f17
cookies4u <jcreyes.1018@gmail.com>
2017-05-19 00:47:26 -0700

    handlebars code

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -5,29 +5,28 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-  // combination of all 3 tables //
   // table of courses with authors
   app.get("/api/courseUser", function(req, res) {
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
+
     // In this case, just db.Post
     db.Course.findAll({
       include: [db.User]
     }).then(function(data) {
-      res.json(data);
+      var courseUser = {
+        Course: data
+      };
+      res.render("courseList", courseUser); 
+
+      //res.json(data);
     });
   });
 
+  // course with reviews and review authors
   app.get("/api/courseReviews", function(req, res) {
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
+
     // WHERE clause needed to show only reivews related to the current course looked at
     db.Course.findAll({ //may need to review to findOne
       where: {id: 1},
-      //include: [db.User],  // need to also include reviews table. may need a nested include like the link i have saved in notes
-      //include: [db.User]
-      // or
       include: [
         {
           model: db.Review,
@@ -36,156 +35,17 @@ module.exports = function(app) {
       ]
 
     }).then(function(data) {
-      res.json(data);
-    });
-  });
 
-/*
-// these returned returns a combination of authors and their courses. using model3
-  // GET route for getting all of the posts
-  app.get("/api/courses", function(req, res) {
-    var query = {}; // need to review the dirrence between using a where statement vs no where statement
-    if (req.query.author_id) { //use userid field may be limiting a user per post
-      query.AuthorId = req.query.author_id;
-    }
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Author
-    db.Course.findAll({
-      where: query,
-      include: [db.User]
-    }).then(function(data) {
-      res.json(data);
-    });
-  });
-
-  app.get("/api/users", function(req, res) {
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
-    db.User.findAll({
-      include: [db.Course]
-    }).then(function(data) {
-      res.json(data);
-    });
-  });
-*/
-
-
-// this one returned single course object at the url. using model2
-/*  
-  app.get("/courses", function(req, res) {
-    db.Course.findAll({})
-    .then(function(data) {
       var courseObject = {
+        //Course: data[0].Reviews
         Course: data
       };
-      res.json(data); //will return json ojbect at / url
-    });
+      res.render("courseReviews", courseObject); //will return json ojbect at / url
 
-  });
-*/
+      //res.json(data);
+      //res.json(data[0].Reviews); // all reviews of course
 
-/*
-  app.get("/", function(req, res) {
-
-    db.User.findOne(
-      {
-        username: req.body.username,
-        password: req.body.password
-
-    })
-    .then(function(data) {
-      var userObject = {
-        User: data
-      };
-      res.render("index", userObject);
     });
   });
-*/
-
-/*
-  app.get("/", function(req, res) {
-    db.User.findAll({})
-    .then(function(data) {
-      var userObject = {
-        User: data
-      };
-      res.render("index", userObject);
-      //res.redirect("/"); // wont work haveing both
-    });
-
-    ///// comment out cant call two dbs at once
-    db.Course.findAll({})
-    .then(function(data) {
-      var courseObject = {
-        Course: data
-      };
-      res.render("index", courseObject);
-    });
-    
-
-  });
-*/
-
-/*
-  // then then didn't work
-  app.get("/", function(req, res) {
-    db.Course.findAll({})
-    .then(function(data) {
-      var courseObject = {
-        Course: data
-      };
-      res.render("index", courseObject);
-    });
-    .then(function() {
-      res.redirect("/");
-    });
-
-  });
-*/
-
-/*
-  app.post("/", function(req, res) {
-    db.Burger.create({
-      burger_name: req.body.burger_name,
-      devoured: req.body.devoured
-    })
-    .then(function(data) {
-      res.redirect("/");
-    });
-  });
-
-
-  app.put("/:id", function(req, res) {
-    //var condition = "id = " + req.params.id;
-    //console.log("condition", condition);
-    db.Burger.update(
-      {
-        devoured: req.body.devoured
-      },
-      {
-        where: {
-          id: req.params.id
-        }
-      })
-    .then(function() {
-      res.redirect("/");
-    });
-  });
-
-
-  app.delete("/:id", function(req, res) {
-    // var condition = "id = " + req.params.id;
-    db.Burger.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-    .then(function() {
-      res.redirect("/");
-    });
-  });
-*/
 
 };
---
+++ jsonOutput/courseUserOut.js
@@ -0,0 +1,102 @@
+[
+	{
+		"id": 4,
+		"course_title": "CSS advanced",
+		"course_cat": "frontend",
+		"course_desc": null,
+		"course_url": null,
+		"progress_length": null,
+		"createdAt": "2017-05-18T00:00:00.000Z",
+		"updatedAt": "2017-05-18T00:00:00.000Z",
+		"UserId": 1,
+		"User": {
+			"id": 1,
+			"username": "juan",
+			"password": "123",
+			"first_name": null,
+			"date": null,
+			"createdAt": "2017-05-18T00:00:00.000Z",
+			"updatedAt": "2017-05-18T00:00:00.000Z"
+		}
+	},
+	{
+		"id": 3,
+		"course_title": "CSS newb",
+		"course_cat": "frontend",
+		"course_desc": null,
+		"course_url": null,
+		"progress_length": null,
+		"createdAt": "2017-05-18T00:00:00.000Z",
+		"updatedAt": "2017-05-18T00:00:00.000Z",
+		"UserId": 2,
+		"User": {
+			"id": 2,
+			"username": "michelle",
+			"password": "123",
+			"first_name": null,
+			"date": null,
+			"createdAt": "2017-05-18T00:00:00.000Z",
+			"updatedAt": "2017-05-18T00:00:00.000Z"
+		}
+	},
+	{
+		"id": 2,
+		"course_title": "html advanced",
+		"course_cat": "frontend",
+		"course_desc": null,
+		"course_url": null,
+		"progress_length": null,
+		"createdAt": "2017-05-18T00:00:00.000Z",
+		"updatedAt": "2017-05-18T00:00:00.000Z",
+		"UserId": 3,
+		"User": {
+			"id": 3,
+			"username": "tae",
+			"password": "123",
+			"first_name": null,
+			"date": null,
+			"createdAt": "2017-05-18T00:00:00.000Z",
+			"updatedAt": "2017-05-18T00:00:00.000Z"
+		}
+	},
+	{
+		"id": 1,
+		"course_title": "html newb",
+		"course_cat": "frontend",
+		"course_desc": null,
+		"course_url": null,
+		"progress_length": null,
+		"createdAt": "2017-05-18T00:00:00.000Z",
+		"updatedAt": "2017-05-18T00:00:00.000Z",
+		"UserId": 4,
+		"User": {
+			"id": 4,
+			"username": "abtin",
+			"password": "123",
+			"first_name": null,
+			"date": null,
+			"createdAt": "2017-05-18T00:00:00.000Z",
+			"updatedAt": "2017-05-18T00:00:00.000Z"
+		}
+	},
+	{
+		"id": 5,
+		"course_title": "jQuery newb",
+		"course_cat": "frontend",
+		"course_desc": null,
+		"course_url": null,
+		"progress_length": null,
+		"createdAt": "2017-05-18T00:00:00.000Z",
+		"updatedAt": "2017-05-18T00:00:00.000Z",
+		"UserId": 4,
+		"User": {
+			"id": 4,
+			"username": "abtin",
+			"password": "123",
+			"first_name": null,
+			"date": null,
+			"createdAt": "2017-05-18T00:00:00.000Z",
+			"updatedAt": "2017-05-18T00:00:00.000Z"
+		}
+	}
+]
